68b78d92e769f083c7351cdc8267d8f28b3d226c
Ivan Loh <ivan@eriad.com>
2015-01-11 21:24:34 +0800

    cleanup

--- sample/spam.js
+++ sample/spam.js
@@ -1,40 +1,52 @@
+'use strict';
+
 var config = require('../config/config'),
-    async  = require('async'),
-    yowsup = (function () {
+    async  = require('async');
 
-                var sys  = require('sys'),
-                    exec = require('child_process').exec,
-                    cmd  = (function () {
-                      var command = "yowsup-cli demos -l " + config.yowsup.login    + ":" +
-                                                             config.yowsup.password + " -s ";
 
-                      return function (to, message) {
-                        return command + to + " \"" + message + "\"";
-                      };
-                    }());
 
-                return function (to, message, done) {
-                  exec(cmd(to, message), function (err, stdout, stderr) {
-                    if (err)    { console.error(err); }
-                    if (stderr) { sys.puts(stderr);   }
-                    sys.puts(stdout);
-                    done();
-                  });
+var yowsup = (function () {
+
+  var sys  = require('sys'),
+      exec = require('child_process').exec,
+      cmd  = (function () {
+                var command = "yowsup-cli demos -l " + config.yowsup.login    + ":" +
+                                                       config.yowsup.password + " -s ";
+
+                return function (to, message) {
+                  return command + to + " \"" + message + "\"";
                 };
-
               }());
 
 
+  return function (to, message, done) {
+    exec(cmd(to, message), function (err, stdout, stderr) {
+      if (err) {
+        console.error(err);
+      }
+      if (stderr) {
+        sys.puts(stderr);
+      }
+      sys.puts(stdout);
+      done();
+    });
+  };
 
 
-var count = 0,
-    LIMIT = 100;
+}());
+
+
+
+
+var count  = 0,
+    LIMIT  = 50,
+    TARGET = "some number";
 
 async.whilst(
   function check() { return count < LIMIT; },
   function exec(done) {
 
-    yowsup("put some poor soul's number here", " Hi! " + Date.now(), function (){
+    yowsup(TARGET, Date.now() + "\tkang!", function (){
       count = count + 1;
       done();
     });
